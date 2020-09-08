a9b9be4bd7209d9c99bc6bc889992f7e5e2b052a
pepe <jjsmm97@gmail.com>
2020-09-08 18:17:46 -0400

    Updates

---
+++ precache-manifest.9ef3f84531616c20be0293312cb91b35.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d796b7651206b8fea8b15e99fab715b",
+    "url": "/JuanJoseMirandaM/juanjose.site/index.html"
+  },
+  {
+    "revision": "5dce10e9e6abf08c0576",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/css/main.b45aaca7.chunk.css"
+  },
+  {
+    "revision": "5f1b41bd60434ce924c8",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/js/2.f686194c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/js/2.f686194c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dce10e9e6abf08c0576",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/js/main.5bb02732.chunk.js"
+  },
+  {
+    "revision": "e89f108e1fa4368f25ef",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/js/runtime-main.f11bacad.js"
+  },
+  {
+    "revision": "05a0e905ff405c118400096ffa61f7b7",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/Flaticon.05a0e905.eot"
+  },
+  {
+    "revision": "0666a52d1f32ff258540a5937b741f5a",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/Flaticon.0666a52d.woff2"
+  },
+  {
+    "revision": "0bed328f9da6afa88af1e56b89c640ec",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/Flaticon.0bed328f.woff"
+  },
+  {
+    "revision": "0e69e22930adfca0ca8e425f16f9c1fd",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/Flaticon.0e69e229.svg"
+  },
+  {
+    "revision": "e6f918d24556f0652cd7c4ac0d0f0899",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/Flaticon.e6f918d2.ttf"
+  },
+  {
+    "revision": "2cb8dc8186e8827cca3b6beebe4ef1c6",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/icomoon.2cb8dc81.eot"
+  },
+  {
+    "revision": "302fbc0aa5dcec40d385772d64baad2c",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/icomoon.302fbc0a.svg"
+  },
+  {
+    "revision": "62f33c03d096cdc058166bd6fbf7e4d0",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/icomoon.62f33c03.woff"
+  },
+  {
+    "revision": "bab8cf81a530d89b36f24a80649c5e25",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/icomoon.bab8cf81.ttf"
+  },
+  {
+    "revision": "1e2d956df26242b9db0edc190652935f",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/inicio.1e2d956d.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "a922fe6c69816a3fd3c995d0f175a96c",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/python.a922fe6c.jpg"
+  },
+  {
+    "revision": "8cf6ad3eca7d5c1a101d9f3df09bef35",
+    "url": "/JuanJoseMirandaM/juanjose.site/static/media/vsc.8cf6ad3e.jpg"
+  }
+]);
